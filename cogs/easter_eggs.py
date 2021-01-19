1648de72215d8eb7f76386b74365c4578aef45d3
Atari2 <alex.robassomero@gmail.com>
2021-01-20 00:03:55 +0100

    Added http cat command

--- cogs/easter_eggs.py
+++ cogs/easter_eggs.py
@@ -75,3 +75,14 @@ class Eggs(commands.Cog, command_attrs={'hidden': True}):
     @commands.command()
     async def invite(self, ctx):
         await ctx.send(f'<{utils.oauth_url("714088423732019232")}>')
+
+    @commands.command()
+    async def cattp(self, ctx, err: int):
+        cat_codes = [100, 101, 102, 200, 201, 202, 203, 204, 205, 206, 207, 208, 226, 300, 301, 302, 303, 304, 305, 307,
+                     308, 400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418,
+                     421, 422, 423, 424, 426, 428, 429, 431, 444, 451, 499, 500, 501, 502, 503, 504, 505, 506, 507, 508,
+                     510, 511, 599]
+        if err not in cat_codes:
+            await ctx.send('No cat was available, sorry :(')
+        else:
+            await ctx.send(f'https://http.cat/{err}')
